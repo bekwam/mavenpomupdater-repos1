80176ef88c3553bde1b7a667fb093ad91986418c
Carl Walker <carl@bekwam.com>
2014-10-15 11:55:29 -0400

    Added key handler

--- mavenpomupdater-application/src/main/java/com/bekwam/mavenpomupdater/Main.java
+++ mavenpomupdater-application/src/main/java/com/bekwam/mavenpomupdater/Main.java
@@ -1,27 +1,40 @@
 package com.bekwam.mavenpomupdater;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.StackPaneBuilder;
 import javafx.stage.Stage;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Main entry point for MPU
+ * 
+ * Scene graph structure
+ * 
+ * Stack Pane --> FlowPane --> GridPane (from alert.fxml)
+ *            +-> VBox (from manenpomupdater.fxml)
  * 
  * @author carlwalker
  * @since 1.1.0
  */
 public class Main extends Application {
 
+	private Log log = LogFactory.getLog(Main.class);
+	
     @Override
     public void start(Stage primaryStage) throws Exception{
     	
-    	StackPane sp = StackPaneBuilder.create().build();
+    	final StackPane sp = StackPaneBuilder.create().build();
     	
     	FXMLLoader mainViewLoader= new FXMLLoader(getClass().getResource("mavenpomupdater.fxml"));
     	Parent mainView = (Parent)mainViewLoader.load();
@@ -30,12 +43,12 @@ public class Main extends Application {
     	FXMLLoader alertViewLoader = new FXMLLoader(getClass().getResource("alert.fxml"));
     	Parent alertView = (Parent)alertViewLoader.load();
     	
-    	AlertController alertController = alertViewLoader.getController();
+    	final AlertController alertController = alertViewLoader.getController();
     	mainViewController.alertController = alertController;
-    	
+
         mainView.getStyleClass().add("main-view-pane");
         
-        FlowPane fp = new FlowPane();
+        final FlowPane fp = new FlowPane();
         fp.setAlignment(Pos.CENTER);
         fp.getChildren().add( alertView );
         fp.getStyleClass().add("alert-background-pane");
@@ -49,6 +62,19 @@ public class Main extends Application {
         
         Scene scene = new Scene(sp);
         scene.getStylesheets().add("com/bekwam/mavenpomupdater/mpu.css");
+        
+        scene.setOnKeyPressed( new EventHandler<KeyEvent>() {
+        	 @Override
+             public void handle(KeyEvent t) {
+                 KeyCode key = t.getCode();
+                 if (key == KeyCode.ESCAPE  && (sp.getChildren().get(1) == fp)) {
+                	 if( log.isDebugEnabled() ) {
+                		 log.debug("[KEY] the escape key was pressed on the alert screen");
+                		 alertController.dismiss();
+                	 }
+                 }
+             }
+        });
         
         primaryStage.setScene(scene);
         primaryStage.show();
