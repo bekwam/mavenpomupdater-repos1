c3428dc1a0d0247a7125dc81efdba2fd477528bf
walkerca <carl@bekwam.com>
2015-03-09 12:07:42 -0400

    [#17 status: ready for next release] Added new tab and DAO for Preferences

---
+++ mavenpomupdater-application/src/main/java/com/bekwam/mavenpomupdater/data/PreferencesConstants.java
@@ -0,0 +1,15 @@
+package com.bekwam.mavenpomupdater.data;
+
+/**
+ * Class containing constants for Preferences Map including keys and default values
+ *
+ * Created by carl_000 on 3/9/2015.
+ */
+public class PreferencesConstants {
+
+    public final static String PREFIX_PREFERENCES = "preferences.";
+
+    public final static String KEY_FILTERS = PREFIX_PREFERENCES + "filters";
+
+    public final static String DEFAULT_FILTERS = "target";
+}
